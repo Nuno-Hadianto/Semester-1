bf5e5d29f3c149d94c7945cb2abfd51a6cee1503
Mohammed Noeno Hadianto <nunohadianto@gmail.com>
2024-01-07 15:23:16 +0800

    Add files via upload

---
+++ biar_berwarna.py
@@ -0,0 +1,17 @@
+# ANSI color codes
+RESET = "\033[0m"
+BOLD = "\033[1m"
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+PURPLE = "\033[95m"
+CYAN = "\033[96m"
+
+print(RED + "" + RESET)
+print(BOLD + "" + RESET)
+print(BOLD + "" + RESET)
+print(BOLD + "" + RESET)
+print(BOLD + "" + RESET)
+
+
